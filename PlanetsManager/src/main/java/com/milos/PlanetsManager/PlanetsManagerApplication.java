d7226ae25b9dab1ee6bcb4b2026ef89372fa7108
Milos <zrnkemiki@gmail.com>
2023-02-25 00:32:38 +0100

    Project init

---
+++ PlanetsManager/src/main/java/com/milos/PlanetsManager/PlanetsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.milos.PlanetsManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetsManagerApplication.class, args);
+	}
+
+}
