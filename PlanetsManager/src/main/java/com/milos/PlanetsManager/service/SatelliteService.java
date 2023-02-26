486ab0489cc5efee22f17e8491d722729e7809dc
Milos <zrnkemiki@gmail.com>
2023-02-26 21:30:14 +0100

    Create Satellite added
    Controller and Service for new Satellite creation added.

--- PlanetsManager/src/main/java/com/milos/PlanetsManager/service/SatelliteService.java
+++ PlanetsManager/src/main/java/com/milos/PlanetsManager/service/SatelliteService.java
@@ -2,12 +2,15 @@ package com.milos.PlanetsManager.service;
 
 import java.util.List;
 
+import com.milos.PlanetsManager.dto.SatelliteDto;
+import com.milos.PlanetsManager.exception.EntityDoesNotExistException;
 import com.milos.PlanetsManager.model.Satellite;
 
 public interface SatelliteService {
-	Satellite saveSatellite(Satellite newSatellite);
+	Satellite saveSatellite(SatelliteDto satelliteDto) throws EntityDoesNotExistException;
 	List<Satellite> fetchSatelliteList();
 	Satellite updateSatellite(Satellite updateSatellite);
 	void deleteSatellitetById(Long satelliteId);
+	Satellite saveSatellite(Satellite newSatellite);
 	
 }
--- PlanetsManager/src/main/java/com/milos/PlanetsManager/serviceImpl/SatelliteServiceImpl.java
+++ PlanetsManager/src/main/java/com/milos/PlanetsManager/serviceImpl/SatelliteServiceImpl.java
@@ -3,8 +3,13 @@ package com.milos.PlanetsManager.serviceImpl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import com.milos.PlanetsManager.dto.SatelliteDto;
+import com.milos.PlanetsManager.exception.EntityAlreadyExistsException;
+import com.milos.PlanetsManager.exception.EntityDoesNotExistException;
+import com.milos.PlanetsManager.model.Planet;
 import com.milos.PlanetsManager.model.Satellite;
 import com.milos.PlanetsManager.repository.SatelliteRepository;
 import com.milos.PlanetsManager.service.SatelliteService;
@@ -14,11 +19,21 @@ public class SatelliteServiceImpl implements SatelliteService {
 
 	@Autowired
 	SatelliteRepository satelliteRepository;
-	
+
+	@Autowired
+	PlanetServiceImpl planetService;
+
 	@Override
-	public Satellite saveSatellite(Satellite newSatellite) {
-		// TODO Auto-generated method stub
-		return null;
+	public Satellite saveSatellite(SatelliteDto satelliteDto) throws EntityDoesNotExistException {
+		Satellite satellite = new Satellite(satelliteDto);
+		if (satelliteDto.getId() != null && satelliteRepository.existsById(satelliteDto.getId())) {
+			throw new EntityAlreadyExistsException(HttpStatus.BAD_REQUEST, "Satellite with given ID already exists!");
+		}
+		Planet planet = planetService.fetchPlanetById(satelliteDto.getPlanetId());
+		planet.getSatellites().add(satellite);
+		planetService.updatePlanet(planet); // Saving the planet, the new satellite will be saved too!
+
+		return satellite;
 	}
 
 	@Override
@@ -37,6 +52,12 @@ public class SatelliteServiceImpl implements SatelliteService {
 	public void deleteSatellitetById(Long satelliteId) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public Satellite saveSatellite(Satellite newSatellite) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
