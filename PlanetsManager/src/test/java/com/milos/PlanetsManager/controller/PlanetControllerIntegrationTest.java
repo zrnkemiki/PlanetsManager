c22e5e7bc4cdbc25036716c73f42e3e08a185a24
Milos <zrnkemiki@gmail.com>
2023-02-28 16:24:44 +0100

    Test for planet creation controller
    Test method for planet integration test controller.

--- PlanetsManager/src/test/java/com/milos/PlanetsManager/controller/PlanetControllerIntegrationTest.java
+++ PlanetsManager/src/test/java/com/milos/PlanetsManager/controller/PlanetControllerIntegrationTest.java
@@ -1,11 +1,19 @@
 package com.milos.PlanetsManager.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashSet;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+
+import com.milos.PlanetsManager.model.Planet;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class PlanetControllerIntegrationTest {
@@ -21,6 +29,19 @@ public class PlanetControllerIntegrationTest {
 	private String createURLWithPort(String uri) {
 		return "http://localhost:" + port + uri;
 	}
+	
+	@Test
+	public void testCreatePlanet() {
+		Planet planet = new Planet(1234L, "Planet test", 111L, 111L, 111L, 111, new HashSet<>());
+		ResponseEntity<String> responseEntity = this.restTemplate.postForEntity("http://localhost:" + port + "/planet",
+				planet, String.class);
+		String expected = "{\"id\":1,\"name\":\"Planet test\",\"surfaceArea\":111,\"mass\":111,\"distanceFromSun\""
+				+ ":111,\"averageSurfaceTemperature\":111,\"satellites\":[]}";
+		assertEquals(expected, responseEntity.getBody());
+		assertEquals(201, responseEntity.getStatusCodeValue());
+
+	}
+
 
 
 	
