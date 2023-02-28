29244d90bfa579ca54d965a980fbe01b6ea65b84
Milos <zrnkemiki@gmail.com>
2023-02-28 16:30:16 +0100

    Test fetch planet by ID
    Test method for fetching planet by ID;

--- PlanetsManager/src/test/java/com/milos/PlanetsManager/controller/PlanetControllerIntegrationTest.java
+++ PlanetsManager/src/test/java/com/milos/PlanetsManager/controller/PlanetControllerIntegrationTest.java
@@ -10,7 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
 import com.milos.PlanetsManager.model.Planet;
@@ -29,31 +31,39 @@ public class PlanetControllerIntegrationTest {
 	private String createURLWithPort(String uri) {
 		return "http://localhost:" + port + uri;
 	}
-	
+
 	@Test
 	public void testCreatePlanet() {
 		Planet planet = new Planet(1234L, "Planet test", 111L, 111L, 111L, 111, new HashSet<>());
-		ResponseEntity<String> responseEntity = this.restTemplate.postForEntity(createURLWithPort("/planet"),
-				planet, String.class);
+		ResponseEntity<String> responseEntity = this.restTemplate.postForEntity(createURLWithPort("/planet"), planet,
+				String.class);
 		String expected = "{\"id\":1,\"name\":\"Planet test\",\"surfaceArea\":111,\"mass\":111,\"distanceFromSun\""
 				+ ":111,\"averageSurfaceTemperature\":111,\"satellites\":[]}";
 		assertEquals(expected, responseEntity.getBody());
 		assertEquals(201, responseEntity.getStatusCodeValue());
 
 	}
-	
+
 	@Test
 	public void testCreatePlanetWithExistingName() {
 		Planet planet = new Planet(1234L, "Earth", 111L, 111L, 111L, 111, new HashSet<>());
-		ResponseEntity<String> responseEntity = this.restTemplate.postForEntity(createURLWithPort("/planet"),
-				planet, String.class);
+		ResponseEntity<String> responseEntity = this.restTemplate.postForEntity(createURLWithPort("/planet"), planet,
+				String.class);
 		String expected = "Planet with given name already exists!";
 		assertEquals(expected, responseEntity.getBody());
 		assertEquals(400, responseEntity.getStatusCodeValue());
 
 	}
 
+	@Test
+	public void testFetchPlanetById() {
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/planet/111"), HttpMethod.GET,
+				entity, String.class);
+		String expected = "{\"id\":111,\"name\":\"Earth\",\"surfaceArea\":123,\"mass\":1111,\"distanceFromSun\":111,\""
+				+ "averageSurfaceTemperature\":11,\"satellites\":[{\"id\":111,\"name\":\"Moon\",\"surfaceArea\":1111,\""
+				+ "mass\":11,\"naturalSatellite\":true}]}";
+		assertEquals(expected, response.getBody());
+	}
 
-
-	
 }
