ec5924573d43ad583bf4cc4cedb8bce065b4892d
Milos <zrnkemiki@gmail.com>
2023-02-28 18:28:53 +0100

    Test create satellite without name
    Test method for creating satellite without name - BAD REQUEST

--- PlanetsManager/src/test/java/com/milos/PlanetsManager/controller/SatelliteControllerIntegrationTest.java
+++ PlanetsManager/src/test/java/com/milos/PlanetsManager/controller/SatelliteControllerIntegrationTest.java
@@ -1,16 +1,22 @@
 package com.milos.PlanetsManager.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import com.milos.PlanetsManager.model.Satellite;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class SatelliteControllerIntegrationTest {
-	
+
 	@LocalServerPort
 	private int port;
 
@@ -23,6 +29,13 @@ public class SatelliteControllerIntegrationTest {
 		return "http://localhost:" + port + uri;
 	}
 
-	
-	
+	@Test
+	public void testCreateSatelliteWithNoName() {
+		Satellite satellite = new Satellite(1L, "", 1111L, 1111L, true);
+		ResponseEntity<String> responseEntity = this.restTemplate.postForEntity(createURLWithPort("/satellite"),
+				satellite, String.class);
+		System.out.println(responseEntity.getBody());
+		assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+	}
+
 }
