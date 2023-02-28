3edb1e3d461e3c89232520d58dddd5a17184eb6d
Milos <zrnkemiki@gmail.com>
2023-02-28 02:45:10 +0100

    Added data.sql for filling database, added H2 DB dependency and app.prop for testing
    data.sql file added and filled with dummy data, so database can be filled at the beginning;
    
    H2 database dependency added to pom.xml
    
    application.poperties added for testing and configured for H2 database

---
+++ PlanetsManager/src/main/resources/data.sql
@@ -0,0 +1,22 @@
+insert into planets(id, average_surface_temperature, distance_from_sun, mass, name, surface_area) values (111,11,111,1111,'Earth Test', 123);
+
+insert into planets(id, average_surface_temperature, distance_from_sun, mass, name, surface_area) values (222,22,222,2222,'Mars Test', 234);
+
+insert into planets(id, average_surface_temperature, distance_from_sun, mass, name, surface_area) values (333,33,333,3333,'Jupiter Test', 345);
+
+insert into satellites(id, mass, name, natural_satellite, surface_area, planet_id) values (111, 11, 'Moon', 'true', 1111, 111);
+
+
+insert into satellites(id, mass, name, natural_satellite, surface_area, planet_id) values (222, 22, 'Phobos', 'true', 2222, 222);
+
+insert into satellites(id, mass, name, natural_satellite, surface_area, planet_id) values (333, 33, 'Deimos', 'true', 2222, 222);
+
+insert into satellites(id, mass, name, natural_satellite, surface_area, planet_id) values (444, 44, 'Io', 'true', 3333, 333);
+
+insert into satellites(id, mass, name, natural_satellite, surface_area, planet_id) values (555, 55, 'Europa', 'true', 5555, 333);
+
+insert into satellites(id, mass, name, natural_satellite, surface_area, planet_id) values (666, 66, 'Ganymede', 'true', 6666, 333);
+
+insert into satellites(id, mass, name, natural_satellite, surface_area, planet_id) values (777, 77, 'Callisto', true, 7777, 333);
+
+insert into satellites(id, mass, name, natural_satellite, surface_area, planet_id) values (888, 88, 'Amalthea', true, 8888, 333);
