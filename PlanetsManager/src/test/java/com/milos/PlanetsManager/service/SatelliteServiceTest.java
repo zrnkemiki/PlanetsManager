e1b60eaaafc7c7144faeb6568c947c58ceae1ebd
Milos <zrnkemiki@gmail.com>
2023-02-28 03:35:50 +0100

    SatelliteServiceTest  init; Test for fetching all satellites
    SatelliteServiceTest class init;
    Test method for fetching all satellites;

---
+++ PlanetsManager/src/test/java/com/milos/PlanetsManager/service/SatelliteServiceTest.java
@@ -0,0 +1,42 @@
+package com.milos.PlanetsManager.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.milos.PlanetsManager.model.Satellite;
+import com.milos.PlanetsManager.repository.SatelliteRepository;
+import com.milos.PlanetsManager.serviceImpl.SatelliteServiceImpl;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class SatelliteServiceTest {
+
+	@MockBean
+	SatelliteRepository satelliteRepository;
+	
+	@Autowired
+	SatelliteServiceImpl satelliteService;
+	
+	@Test
+	public void fetchSatellites() {
+		Satellite moonSatellite = new Satellite(1L, "Moon", 1111L, 1111L, true);
+		Satellite phobosSatellite = new Satellite(2L, "Phobos", 2222L, 2222L, true);
+		List<Satellite> satellites = new ArrayList<>();
+		satellites.add(moonSatellite);
+		satellites.add(phobosSatellite);
+		
+		when(satelliteRepository.findAll()).thenReturn(satellites);
+		assertEquals(satellites, satelliteService.fetchSatellits());
+		
+	}
+}
